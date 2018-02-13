3fe2a3e18076b2d2ba4b460b2ea782767325bdae
Arnulfo <perezalvareza.271@gmail.com>
2018-02-13 17:54:40 -0600

    nuevos cambios
    nuevo formulario

--- pagina.php
+++ pagina.php
@@ -1,39 +1,88 @@
 <html>
 	
 	<head>
-		<title>test.html</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-theme.css" rel="stylesheet">
-		<script src="js/jquery-3.1.1.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>	
-
-	</head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bootstrap Form with jQuery Validations</title>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
+</head>
+	
 
 
 
 
 <body>
 
+ <div class="container">
 
-	<div id="body">
+    <div class="signup-form-container">
+    
+         <!-- form start -->
+         <form role="form" id="register-form" autocomplete="off">
+         
+         <div class="form-header">
+          <h3 class="form-title"><i class="fa fa-user"></i> Sign Up</h3>
+                      
+         <div class="pull-right">
+             <h3 class="form-title"><span class="glyphicon glyphicon-pencil"></span></h3>
+         </div>
+                      
+         </div>
+         
+         <div class="form-body">
+                      
+            <div class="form-group">
+                   <div class="input-group">
+                   <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
+                   <input name="name" type="text" class="form-control" placeholder="Username">
+                   </div>
+                   <span class="help-block" id="error"></span>
+              </div>
+                        
+              <div class="form-group">
+                   <div class="input-group">
+                   <div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></div>
+                   <input name="email" type="text" class="form-control" placeholder="Email">
+                   </div> 
+                   <span class="help-block" id="error"></span>                     
+              </div>
+                        
+              <div class="row">
+                        
+                   <div class="form-group col-lg-6">
+                        <div class="input-group">
+                        <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
+                        <input name="password" id="password" type="password" class="form-control" placeholder="Password">
+                        </div>  
+                        <span class="help-block" id="error"></span>                    
+                   </div>
+                            
+                   <div class="form-group col-lg-6">
+                        <div class="input-group">
+                        <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></div>
+                        <input name="cpassword" type="password" class="form-control" placeholder="Retype Password">
+                        </div>  
+                        <span class="help-block" id="error"></span>                    
+                   </div>
+                            
+             </div>
+                        
+                        
+            </div>
+            
+            <div class="form-footer">
+                 <button type="submit" class="btn btn-info">
+                 <span class="glyphicon glyphicon-log-in"></span> Sign Me Up !
+                 </button>
+            </div>
 
-<form method="get" action="post">
 
-Nombre: <input type="text" name="nombre"><br>
+            </form>
+            
+           </div>
 
-Apellidos: <input type="text" name="apellidos"><br>
+ </div>
+    
 
-Dirección: <input type="text" name="direccion"><br>
-
-Correo electrónico: <input type="text" name="correo"><br>
-
-Teléfono: <input type="text" name="telefono" ><br>
-
-</form>
-
-</div>
-	
 </body>
 
 </html>
