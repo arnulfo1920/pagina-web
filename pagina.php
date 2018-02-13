78bf88f8afe4159916f759b94519a98b58281c70
Arnulfo <perezalvareza.271@gmail.com>
2018-02-13 13:51:05 -0600

    nuevo comit

--- pagina.php
+++ pagina.php
@@ -1,1 +1,32 @@
-pagina.php
+<html>
+	
+	<head>
+		<title>index.html</title>
+
+
+	</head>
+
+
+
+
+<body>
+
+<form action="conexion.php" method="post">
+	<div class="table table-bordered">
+	</div>
+	<div>
+		<tr>
+			
+			<th>nombre:</th>
+			<th>apellido</th>
+			<th>direccion</th>
+			<th>email</th>
+		</tr>
+
+	</div>
+
+
+	
+</body>
+
+</html>
